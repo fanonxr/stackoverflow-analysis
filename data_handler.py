de5722dc11fb8e2c48d59b9d45596b1e670c0c75
FanonX Rogers <fanonxdevelops@gmail.com>
2019-10-25 20:07:43 -0400

    further analyzing: group which type of developers work with specific languages.

--- data_handler.py
+++ data_handler.py
@@ -32,3 +32,47 @@ def get_hobbyist_count(reader):
 
     return counts
 
+
+def get_language(reader):
+    """
+    function to get the most popular language
+    :param reader:
+    :return: a list containing the Counter collection and the total amount of people using that language
+    """
+
+    language_counter = Counter()
+    total = 0
+
+    for line in reader:
+        # get all the langauages that the developer has worked with
+        languages = line['LanguageWorkedWith'].split(";")
+
+        # loop over the list of languages and increment the counter
+        language_counter.update(languages)
+        total += 1
+
+    return [language_counter, total]
+
+
+def get_type_of_developer(reader):
+    """
+    :params: reader csv generator object containing the contents of the csv file
+    :return: 
+    """
+    language_counter = Counter()
+    total = 0
+    dev_type_info = {}
+    
+    for line in reader:
+        dev_types = line['DevType'].split(';')
+        
+        for dev_type in dev_types:
+            # set default checks to see if we have a value for that key, (dev type)
+            dev_type_info.setdefault(dev_type, {
+                'total': 0,
+                'language_counter': Counter()
+            })
+
+            languages = line['LanguageWorkedWith'].split(';')
+            dev_type_info[dev_type]['language_counter'].update(languages)
+            dev_type_info[dev_type]['total'] += 1
